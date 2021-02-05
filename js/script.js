8fe873e8a0d4a20aeadafc436cf85ee8d39742eb
sabrina-fontana <sabfon@hotmail.it>
2021-02-05 14:15:13 +0100

    aggiunto css e metodi

--- js/script.js
+++ js/script.js
@@ -4,13 +4,25 @@ new Vue({
     emailsArray: []
   },
   mounted() {
-    let that = this;
-    for (var x = 0; x < 10; x++) {
-      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-      .then(function(resp) {
-        console.log(resp.data.response)
-        that.emailsArray.push(resp.data.response);
-      });
+    this.mountedFunction()
+  },
+  methods: {
+    mountedFunction: function() {
+      let that = this;
+      for (var x = 0; x < 10; x++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(function(resp) {
+          console.log(resp.data.response)
+          that.emailsArray.push(resp.data.response);
+        });
+      }
+    },
+    deleteEmail: function(index) {
+      this.emailsArray.splice(index, 1)
+    },
+    createEmails: function() {
+      this.emailsArray = [];
+      this.mountedFunction()
     }
   }
 })
